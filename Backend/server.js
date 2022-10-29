4505e9d2585e054fc3d538e4de0f437e7c3f8cd3
halim <yahiaouihlm@gmail.com>
2022-10-29 19:09:29 +0200

    connect to  MongoDB database

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userRoutes = require('./routes/user.routes');
+//const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' });
 require("./config/db");
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 
 
 //routes
-app.use('/api.user', userRoutes);
+//app.use('/api.user', userRoutes);
 
 
 
