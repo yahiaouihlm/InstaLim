9fc15593e9a51dff272268bf8c7e4d2676e3cfb0
halim <yahiaouihlm@gmail.com>
2022-10-29 18:55:25 +0200

    project initialization

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userRoutes = require('./routes/user.routes');
+require('dotenv').config({ path: './config/.env' });
+require("./config/db");
+const app = express();
+
+
+
+
+
+
+//routes
+app.use('/api.user', userRoutes);
+
+
+
+//server
+app.listen(process.env.PORT, () => {
+    console.log(`Listing on port ${process.env.PORT}`);
+})
