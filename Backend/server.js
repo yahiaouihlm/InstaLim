e97936f9b5bc0701aedd5ba7812b293093b1fe73
halim <yahiaouihlm@gmail.com>
2022-10-30 21:27:24 +0100

    query filtering with body-parser

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-//const userRoutes = require('./routes/user.routes');
+const bodyparser = require('body-parser');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' });
 require("./config/db");
 const app = express();
 
 
 
-
-
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 //routes
-//app.use('/api.user', userRoutes);
+app.use('/api/user', userRoutes);
 
 
 
