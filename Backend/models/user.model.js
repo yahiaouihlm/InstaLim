dd38b9e7b33cfff4af0dab511d15578577e4ef4a
halim <yahiaouihlm@gmail.com>
2022-10-30 21:09:36 +0100

    manage the timestamps problem

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema(
     {
         pseudo: {
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema(
             unique: true,
             trimp: true,
         },
-
         email: {
             type: String,
             require: true,
@@ -26,7 +25,10 @@ const userSchema = new mongoose.Schema(
             max: 1024,
             min: 6
         },
-
+        picture: {
+            type: String,
+            default: './uploads/profil/random-user.png'
+        },
         bio: {
             type: String,
             max: 1024,
@@ -41,10 +43,28 @@ const userSchema = new mongoose.Schema(
         },
 
         likes: {
-            type: [String]
+            type: [String],
         },
+
+
+    },
+    {
         timestamps: true,
     }
-)
+);
+
+
+
+//filtrages des données avant les enregistré dans la base des données 
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+
+
 const UserModel = mongoose.model('user', userSchema);
-module.exports = UserModel;  
+module.exports = UserModel;
+
+
